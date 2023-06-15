9e164b5ad5baf10a786d6bfec53229a3c376328b
taqinur <taqinur@biznovelty.com>
2023-06-15 14:52:44 +0600

    DONE

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 const app:Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 import app from './app'
 import config from './config/index'
 
-async function main() {
+async function bootstrap() {
     try {
     await mongoose.connect(config.database_url as string);
+    console.log('database connection established')
     app.listen(config.port, () => {
         console.log(`Example app listening on port ${config.port}`)
       })
@@ -12,3 +13,5 @@ async function main() {
     console.log("Failed to connect to DB", e)
 }
 }
+
+bootstrap();
