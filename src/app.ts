964e20470314bb5cfce87b1649425b6868a3acfd
taqinur <taqinur@biznovelty.com>
2023-06-20 12:58:34 +0600

    added husky and setup

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,14 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app:Application = express()
-const port = 3000
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-export default app;
+export default app
