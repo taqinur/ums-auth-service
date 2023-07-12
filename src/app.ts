cbc8e24116db8e344c6e84c3a864ebf0735a9340
Taqi <taqi1662@gmail.com>
2023-07-12 15:14:19 +0600

    improve linting

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -7,8 +8,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/api/v1/users', usersRouter)
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Working successfully')
 })
 
 export default app
