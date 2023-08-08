62daed867ce801d3cf5083daa0301a315f27894d
Taqi <taqi1662@gmail.com>
2023-08-08 13:01:10 +0600

    zod validation set

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express'
 import cors from 'cors'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/users/user.route'
+import { UserRoutes } from './app/modules/user/user.route'
 
 const app: Application = express()
 
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/v1/users', UserRoutes.router)
+app.use('/api/v1/users', UserRoutes)
 
 // Testing
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
