4e9b2779255f3853bb53f1ed08c3468f1b624499
Taqi <taqi1662@gmail.com>
2023-08-12 12:54:18 +0600

    not found route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import { router as routes } from './app/routes';
+import httpStatus from 'http-status';
 
 const app: Application = express();
 
@@ -21,4 +22,18 @@ app.use('/api/v1/', routes);
 // Global error Handler
 app.use(globalErrorHandler);
 
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API not found',
+      },
+    ],
+  });
+  next();
+});
 export default app;
